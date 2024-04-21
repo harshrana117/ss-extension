c2e2eb83a0566f87ff443398062d25bc48b9df1e
Harsh Rana <harshrana117@gmail.com>
2024-04-22 02:03:51 +0530

    ss taking and save feature

--- contentScript.js
+++ contentScript.js
@@ -5,8 +5,5 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 function captureFullPage() {
-  chrome.tabs.captureVisibleTab(null, { format: "png" }, function (dataUrl) {
-    console.log("dataUrl", dataUrl);
-    sendResponse(dataUrl);
-  });
+  console.log("Hello");
 }
